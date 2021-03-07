6ff1bec020a9f228962bc950d3145d719fca6040
MissCoconut <hamutailang0101@yeah.net>
2021-03-07 18:08:02 +0800

    init
    Signed-off-by: MissCoconut <hamutailang0101@yeah.net>

--- base/install.sh
+++ base/install.sh
@@ -124,7 +124,7 @@ REPLACE="
 print_modname() {
   ui_print " "
   ui_print "    ********************************************"
-  ui_print "    *         MagiskFrida by AeonLucid         *"
+  ui_print "    *         Frida Installer by MissCoconut   *"
   ui_print "    ********************************************"
   ui_print " "
 }
@@ -147,7 +147,8 @@ set_permissions() {
   set_perm_recursive $MODPATH 0 0 0755 0644
 
   # Custom permissions
-  set_perm $MODPATH/system/xbin/frida-server 0 2000 0755 u:object_r:system_file:s0
+  set_perm $MODPATH/system/bin/frida-server-32 0 2000 0755 u:object_r:system_file:s0
+  set_perm $MODPATH/system/bin/frida-server-64 0 2000 0755 u:object_r:system_file:s0
 }
 
 # You can add more functions to assist your custom script code
--- build.py
+++ build.py
@@ -28,7 +28,7 @@ def download_file(url, path):
     if os.path.exists(path):
         return
 
-    r = requests.get(url, allow_redirects=True)
+    r = requests.get(url, allow_redirects = True)
     with open(path, 'wb') as f:
         f.write(r.content)
 
@@ -52,15 +52,15 @@ def extract_file(archive_path, dest_path):
 def create_module_prop(path, frida_release):
     # Create module.prop file.
     module_prop = """id=magiskfrida
-name=MagiskFrida
+name=InstallFrida
 version=v{0}
 versionCode={1}
-author=AeonLucid
-description=Runs frida-server on boot as root with magisk.
-support=https://github.com/AeonLucid/MagiskFrida/issues
+author=MissCoconut
+description=install frida-server to /system/bin directory
+support=None
 minMagisk=1530""".format(frida_release, frida_release.replace(".", ""))
 
-    with open(os.path.join(path, "module.prop"), "w", newline='\n') as f:
+    with open(os.path.join(path, "module.prop"), "w", newline = '\n') as f:
         f.write(module_prop)
 
 
@@ -84,15 +84,11 @@ def create_module(platform, frida_release):
     # Create module.prop.
     create_module_prop(module_dir, frida_release)
 
-    # Download frida-server archives.
-    frida_download_url = "https://github.com/frida/frida/releases/download/{0}/".format(frida_release)
-    frida_server = "frida-server-{0}-android-{1}.xz".format(frida_release, platform)
-    frida_server_path = os.path.join(PATH_DOWNLOADS, frida_server)
-
-    download_file(frida_download_url + frida_server, frida_server_path)
-
-    # Extract frida-server to correct path.
-    extract_file(frida_server_path, os.path.join(module_dir, "system/xbin/frida-server"))
+    _frida32 = os.path.join(PATH_DOWNLOADS, "frida-server-14.2.13-android-arm")
+    _frida64 = os.path.join(PATH_DOWNLOADS, "frida-server-14.2.13-android-arm64")
+    os.mkdir(os.path.join(module_dir, "system/bin/"))
+    shutil.copyfile(_frida32, os.path.join(module_dir, "system/bin/frida-server-32"))
+    shutil.copyfile(_frida64, os.path.join(module_dir, "system/bin/frida-server-64"))
 
     # Create flashable zip.
     print("Building Magisk module.")
@@ -111,7 +107,7 @@ def create_module(platform, frida_release):
                 print("File {0} does not exist..".format(path))
                 continue
 
-            zf.write(path, arcname=file_name)
+            zf.write(path, arcname = file_name)
 
 
 def main():
@@ -122,17 +118,7 @@ def main():
     if not os.path.exists(PATH_BUILDS):
         os.makedirs(PATH_BUILDS)
 
-    # Fetch frida information.
-    frida_releases_url = "https://api.github.com/repos/frida/frida/releases/latest"
-    frida_releases = requests.get(frida_releases_url).json()
-    frida_release = frida_releases["tag_name"]
-
-    print("Latest frida version is {0}.".format(frida_release))
-
-    # Create flashable modules.
-    create_module("arm", frida_release)
-    create_module("arm64", frida_release)
-
+    create_module("arm_all", "14.2.13")
     print("Done.")
 
 
